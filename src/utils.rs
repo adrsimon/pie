a257b07a5a6e6f1215fc994a6c91c9277840b955
Adrien Simon <adrisim2001@gmail.com>
2024-07-19 11:34:35 +0200

    feat: create a task management system

---
+++ src/utils.rs
@@ -0,0 +1,81 @@
+use std::future::Future;
+use std::sync::atomic::AtomicUsize;
+use bytes::Bytes;
+use flate2::bufread::GzDecoder;
+use semver::{BuildMetadata, Prerelease, Version};
+use tar::Archive;
+use tokio::task::JoinHandle;
+use crate::errors::CommandError;
+
+pub const REGISTRY_URL: &str = "https://registry.npmjs.org";
+
+pub const EMPTY_VERSION: Version = Version {
+    major: 0,
+    minor: 0,
+    patch: 0,
+    pre: Prerelease::EMPTY,
+    build: BuildMetadata::EMPTY,
+};
+
+pub const LATEST: &str = "latest";
+
+pub fn extract_tarball(bytes: Bytes, destination: String) -> Result<(), CommandError> {
+    let bytes = &bytes.to_vec()[..];
+    let gz = GzDecoder::new(bytes);
+    let mut archive = Archive::new(gz);
+
+    archive.unpack(&destination).map_err(CommandError::ExtractionFailed).expect("Failed to extract tarball");
+
+    Ok(())
+}
+
+pub static ACTIVE_TASKS: AtomicUsize = AtomicUsize::new(0);
+
+pub struct TaskAllocator;
+impl TaskAllocator {
+    pub fn add_task<T>(future: T) -> JoinHandle<T::Output>
+    where
+        T: Future + Send + 'static,
+        T::Output: Send + 'static,
+    {
+        tokio::spawn(async move {
+            Self::increment_tasks();
+            let task_result = future.await;
+            Self::decrement_tasks();
+
+            task_result
+        })
+    }
+
+    pub fn add_blocking_task<F, R>(f: F) -> JoinHandle<R>
+    where
+        F: FnOnce() -> R + Send + 'static,
+        R: Send + 'static,
+    {
+        tokio::task::spawn_blocking(move || {
+            Self::increment_tasks();
+            let task_result = f();
+            Self::decrement_tasks();
+
+            task_result
+        })
+    }
+
+    pub fn block_until_done() {
+        while Self::active_tasks() != 0 {
+            std::thread::sleep(std::time::Duration::from_millis(1));
+        }
+    }
+
+    fn increment_tasks() {
+        ACTIVE_TASKS.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+    }
+
+    fn decrement_tasks() {
+        ACTIVE_TASKS.fetch_sub(1, std::sync::atomic::Ordering::SeqCst);
+    }
+
+    fn active_tasks() -> usize {
+        ACTIVE_TASKS.load(std::sync::atomic::Ordering::SeqCst)
+    }
+}
